38bff9a3965a00e07538b9cab9423d476c9763cf
cahcnc91 <camilanierocosta@hotmail.com>
2021-01-12 18:32:42 -0500

    support for tailwind in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,29 @@
+const path = require("path");
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+  webpackFinal: async (config) => {
+    (config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(ts|tsx)$/,
+        include: [path.resolve(__dirname, "..")],
+        use: [
+          {
+            loader: require.resolve("babel-loader"),
+            options: {
+              presets: [require.resolve("babel-preset-react-app")],
+            },
+          },
+          require.resolve("react-docgen-typescript-loader"),
+        ],
+      },
+    ]),
+      config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,1 @@
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+import "../src/assets/css/main.css";
