204dc141e11c3ca6de2314eee260fa01e3e5bdd6
cahcnc91 <camilanierocosta@hotmail.com>
2021-01-19 08:11:55 -0500

    fix: add extend theme and prestorybook script

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,35 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        tiny: "9px",
+      },
+      colors: {
+        primary: {
+          base: "#0968C3",
+          shadow: "#064785",
+          accent: "#4DA4FA",
+          "accent-hover": "#1B8BF9",
+          highlight: "#F7FBFF",
+          "highlight-light": "#F4FAFE",
+          "primary-light": "#D4E7FA",
+          overlay: "#1C73CA",
+        },
+        secondary: {
+          base: "#38657f",
+        },
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+      }),
+      borderColor: (theme) => ({
+        ...theme("colors"),
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@msk/tailwind-config")],
+  plugins: [],
 };
